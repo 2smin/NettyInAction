4f23dd02cfbc8ca7cd18421911e5510dd01af2f2
2smin <planner263@gmail.com>
2023-06-08 21:32:11 +0900

    add test code

--- src/main/java/Codec/Decoder/Client.java
+++ src/main/java/Codec/Decoder/Client.java
@@ -1,26 +1,27 @@
 package Codec.Decoder;
 
-import BootStrap.BootstrapContainer;
-import BootStrap.BootstrapManager;
+import BootStrap.ClinetBootStrapManager;
+import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 
 public class Client {
 
     public static void main(String[] args) throws Exception{
 
-        BootstrapManager bootstrapManager = new BootstrapManager();
+        ClinetBootStrapManager bootstrapManager = new ClinetBootStrapManager();
 
-        bootstrapManager.runServerBootStrap(33335);
+//        ServerBootstrap server = bootstrapManager.runServerBootStrap(
+//                33335, new ToMessageDecoder(), new LastInboundHandler());
+
+//        Thread.sleep(1000); // server client thread 분리되어있으므로 containermap에 저장되기 전에 get 시도. thread sleep으로 시간차 둠
+        //sync 로 스레드 블로킹 처리 후 container map에서 get
+//        Channel serverChannel = BootstrapContainer.getInstance().get(String.valueOf(33335));
+//        serverChannel.pipeline().addLast(new ToMessageDecoder()).addLast(new LastInboundHandler());
+
         ChannelFuture clientChannelFuture = bootstrapManager.runClientBootStrap(33335);
 
-        Thread.sleep(1000); // server client thread 분리되어있으므로 containermap에 저장되기 전에 get 시도. thread sleep으로 시간차 둠
-        //sync 로 스레드 블로킹 처리 후 container map에서 get
-        Channel serverChannel = BootstrapContainer.getInstance().get(String.valueOf(33335));
-
-        serverChannel.pipeline().addLast(new ToMessageDecoder()).addLast(new LastInboundHandler());
 
         ByteBuf buf = Unpooled.buffer();
         buf.writeBytes("A".getBytes());
--- src/main/java/Codec/Decoder/Client2.java
+++ src/main/java/Codec/Decoder/Client2.java
@@ -1,4 +1,41 @@
 package Codec.Decoder;
 
+import BootStrap.ClinetBootStrapManager;
+import BootStrap.ServerBootStrapManager;
+import Codec.CombinedByteStringCodec;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+
 public class Client2 {
+
+    public static void main(String[] args) throws Exception{
+
+        ServerBootStrapManager serverBootStrapManager = ServerBootStrapManager.ServerBootStrapManagerHolder.instance;
+
+        serverBootStrapManager.runServerBootstrap();
+        serverBootStrapManager.addPipeLine(
+                new CombinedByteStringCodec()
+        );
+        serverBootStrapManager.bindServerSocket(33335);
+
+        ClinetBootStrapManager clientBootStrapManager = new ClinetBootStrapManager();
+        ChannelFuture clientChannelFuture = clientBootStrapManager.runClientBootStrap(33335);
+
+        ByteBuf buf = Unpooled.buffer();
+        buf.writeBytes("A".getBytes());
+
+        for(int i=0; i<10; i++){
+            ByteBuf copiedBuf = buf.copy();
+            clientChannelFuture.channel().writeAndFlush(copiedBuf);
+            System.out.println("send: " + i);
+            Thread.sleep(1000);
+        }
+
+        ByteBuf buf2 = Unpooled.buffer();
+        buf2.writeBytes("BBB".getBytes());
+
+        clientChannelFuture.channel().writeAndFlush(buf2);
+    }
+
 }
